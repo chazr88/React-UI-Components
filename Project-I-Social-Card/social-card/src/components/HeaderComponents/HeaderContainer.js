4453131caf9b4f12b69f49c1080950d41bf48f06
Chaz Rowe <chazr_88@hotmail.com>
2019-05-08 13:06:01 -0400

    First push

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail'
+
+function HeaderContainer() {
+    return (
+    <header class="HeaderContainer">
+        <ImageThumbnail />
+        <HeaderTitle />
+        <HeaderContent />
+    </header>
+    )
+}
+
+export default HeaderContainer;
